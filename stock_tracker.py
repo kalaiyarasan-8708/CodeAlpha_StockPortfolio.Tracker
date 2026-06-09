90985db660026cf6b3ae278af8f6c06049d35180
kalaiyarasan-8708 <ajuskalai@gmail.com>
2026-06-09 21:09:34 +0530

    Create stock_tracker.py

---
+++ stock_tracker.py
@@ -0,0 +1,15 @@
+# Stock Portfolio Tracker
+
+A simple Python project that calculates the total investment value of stocks.
+
+Features:
+- User enters stock name
+- User enters quantity
+- Calculates total investment value
+
+Technologies Used:
+- Python
+- Dictionary
+- Input/Output
+
+Author: Kalaiyarasan
